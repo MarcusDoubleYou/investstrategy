27ca339b8363714d3517a5fc4a5ca9fe013ebdd8
Marcus Wachsmuth <marcus.wachsmuth@atomrain.com>
2020-10-01 11:02:36 -0700

    wip finding resistance

--- strategy/test/test_find_resistance.py
+++ strategy/test/test_find_resistance.py
@@ -8,42 +8,15 @@ from strategy.feeder import MockEmitter
 from scipy.signal import find_peaks
 
 import numpy as np
-import pandas as pd
+import matplotlib.pyplot as plt
+
+import numpy as np
 from scipy.signal import argrelextrema
+import pandas as pd
 import matplotlib.pyplot as plt
 
 
-class EtfsTests(unittest.TestCase):
-
-    def test_emitter_loaded_data(self):
-        data = pd.read_csv("resources/aapl::2018-06-01::1min.csv")
-        emitter = MockEmitter(data=data)
-        while emitter.not_finished():
-            self.assertIsNotNone(emitter.emit())
-            # print(emitter.tail())
-
-    def test_add_rsi_macd(self):
-        data = pd.read_csv("resources/ugaz-1min.csv")
-        data = preparedata.add_macd(data)
-        # data = data[100:300]
-        data = data[-100:]
-        x = np.arange(100)
-
-        plt.subplot(3, 1, 1)
-        plt.plot(data['last'], linewidth=.70, c="black")
-        plt.title('price')
-
-        plt.subplot(3, 1, 2)
-        plt.bar(x, data['macd_diff'], alpha=0.3)
-        plt.plot(x, data['macd_signal'], linewidth=.70, c="orange")
-        plt.plot(x, data['macd'], linewidth=.70, c="blue")
-        plt.xlabel('mac d')
-
-        plt.subplot(3, 1, 3)
-        plt.plot(data['rsi'], linewidth=.70, c="black")
-        plt.title('rsi')
-
-        plt.show()
+class TestResistance(unittest.TestCase):
 
     def test_cal_resistance(self):
         data = pd.read_csv("resources/ugaz-1min.csv")
@@ -52,11 +25,14 @@ class EtfsTests(unittest.TestCase):
         data = data[-100:]
         x = np.arange(100)
 
+    # https://stackoverflow.com/questions/31070563/find-all-local-maxima-and-minima-when-x-and-y-values-are-given-as-numpy-arrays?noredirect=1&lq=1
+
     def test_cal_local_min_max(self):
         # https://stackoverflow.com/questions/31070563/find-all-local-maxima-and-minima-when-x-and-y-values-are-given-as-numpy-arrays?noredirect=1&lq=1
-        x = np.array([6, 3, 5, 2, 1, 4, 9, 7, 8])
-        y = np.linspace(1, len(x) + 1, num=len(x) + 1)
-        # y = np.array([2, 1, 3, 5, 3, 9, 8, 10, 7])
+        # x = np.array([6, 3, 5, 2, 1, 4, 9, 7, 8])
+        # y = np.linspace(1, len(x) + 1, num=len(x) + 1)
+        y = np.array([2, 1, 3, 5, 3, 9, 8, 10, 7])
+        x = np.linspace(1, len(y), num=len(y))
 
         # sort the data in x and rearrange y accordingly
         sortId = np.argsort(x)
@@ -79,10 +55,45 @@ class EtfsTests(unittest.TestCase):
         plt.plot(peaks, y[peaks], "x")
         plt.show()
 
+    def test_find_resistance(self):
+        data = pd.read_csv("resources/ugaz-1min.csv")
+        data = data[-100:]
+        peaks = find_peak_values(data['last'].values)
+        print(peaks)
 
-def find_peak(prices):
-    x = prices
-    y = np.linspace(1, len(prices) + 1, num=len(prices) + 1)
+    def test_show_resistance(self):
+        data = pd.read_csv("resources/ugaz-1min.csv")
+        y = data[-100:]
+        y = y['last'].values
+        peaks = find_peak_values(y)
+        resistances = find_resistance(y[peaks], 0.05)
+
+        x = np.linspace(1, len(y), num=len(y))
+        plt.plot(x - 1, y)
+        for r in resistances:
+            plt.axhline(y=r.mean(), xmin=0.0, xmax=1.0, color='r')
+        plt.plot(peaks, y[peaks], "x")
+        plt.show()
+        print(peaks)
+
+
+def find_peak_values(prices):
+    y = prices
+    x = np.linspace(1, len(prices), num=len(prices))
+
+    # sort the data in x and rearrange y accordingly
+    sortId = np.argsort(x)
+    x = x[sortId]
+    y = y[sortId]
+
+    # this way the x-axis corresponds to the index of x
+    plt.plot(x - 1, y)
+    # plt.show()
+    maxm = argrelextrema(y, np.greater)  # (array([1, 3, 6]),)
+    minm = argrelextrema(y, np.less)  # (array([2, 5, 7]),)
+
+    peaks, _ = find_peaks(y)
+    return peaks
 
 
 def find_resistance(peaks, resistance_level_spread=1):
@@ -114,18 +125,18 @@ class Resistance:
     def __init__(self, initial_value) -> None:
         super().__init__()
         self.initial_value = initial_value
-        self.touch_points.append(initial_value)
+        self.touch_points = [initial_value]
 
     def add_value(self, point):
         self.touch_points.append(point)
 
-    def get_mean_resistance(self):
+    def mean(self):
         return np.mean(np.array(self.touch_points))
 
-    def get_upper_lower_limit(self):
+    def upper_lower_limit(self):
         pass
 
-    def get_touch_points(self):
+    def touch_points(self):
         return len(self.touch_points)
 
 # simple way of finding support lines/ area needs to be touched at least three times
